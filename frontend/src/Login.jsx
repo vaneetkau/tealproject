148a4f1b91bba92b2a423e765c1a259b1042268c
Noyal Bishnoi <ngodara01@mylangara.ca>
2023-09-29 20:45:36 -0700

    User Authentication

---
+++ frontend/src/Login.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+
+export const Login = () => {
+    const [email,setEmail] = useState('');
+    const [pass, setPass] = useState('');
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        console.log(email);
+        console.log(pass);
+    }
+    const handleEmailOnChange = (e) =>{
+        e.preventDefault();
+        console.log("On Change");
+        setEmail(e.target.value);
+    }
+    const handlePassOnChange = (e) =>{
+        e.preventDefault();
+        console.log("On Change");
+        setPass(e.target.value);
+    }
+    return(
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="email">Email</label>
+            <input value={email} onChange={handleEmailOnChange} type='email' placeholder="Enter your email here" id="email" name="email" />
+            <label htmlFor="password">Password</label>
+            <input value={pass} onChange={handlePassOnChange} type='password' placeholder="********" id="password" name ="password" />
+            <button type="submit">Login</button>
+        </form>
+    )
+}
