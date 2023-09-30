56d9183d20d79554b1564c9e51b632a420c63cb7
Noyal Bishnoi <ngodara01@mylangara.ca>
2023-09-29 21:48:14 -0700

    User Authentication

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,20 @@
+import React, { useState } from "react";
 import logo from './logo.svg';
 import './App.css';
 import { Login } from './Login';
 import { Register } from './Register';
 
 function App() {
+  const [currentForm, setCurrentForm] = useState('login');
+
+  const toggleForm = (forName) => {
+    setCurrentForm(forName);
+  }
   return (
     <div className="App">
-      <Login/>
-      <Register/>
+      {
+        currentForm === "login" ? <Login onFormSwitch={toggleForm}/> : <Register onFormSwitch={toggleForm}/>
+      }
   </div>
   );
 }
--- frontend/src/Register.jsx
+++ frontend/src/Register.jsx
@@ -1,5 +1,38 @@
-export const Register = () => {
+import React, { useState } from "react";
+
+export const Register = (props) => {
+    const [email, setEmail] = useState("");
+    const [pass, setPass] = useState("");
+    const [name, setName] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(email);
+        console.log(pass);
+    };
+    const handleEmailOnChange = (e) => {
+        e.preventDefault();
+        console.log("On Change");
+        setEmail(e.target.value);
+    };
+    const handlePassOnChange = (e) => {
+        e.preventDefault();
+        console.log("On Change");
+        setPass(e.target.value);
+    };
+
     return(
-        <>Register</>
+        <>
+            <form onSubmit={handleSubmit}>
+                <label>Full Name</label>
+                <input value={name} onChange={handleEmailOnChange} name="name" id="name" placeholder="Full Name"/>
+                <label htmlFor="email">Email</label>
+                <input value={email} onChange={handleEmailOnChange} type="email" placeholder="Enter your email here" id="email" name="email"/>
+                <label htmlFor="password">Password</label>
+                <input value={pass} onChange={handlePassOnChange} type="password" placeholder="********" id="password" name="password"/>
+                <button type="submit">Login</button>
+            </form>
+            <button onClick={() => props.onFormSwitch('login')}>Already have an account? Login here</button>
+        </>
     )
 }
