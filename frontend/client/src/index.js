2930a45bdf51b705ed61ed0821f3b7e0312b81d2
vaneetkau <vkaur24@mylangara.ca>
2023-10-26 10:24:07 +0530

    add components in  the projects

--- frontend/client/src/index.js
+++ frontend/client/src/index.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+//import App from './App';
+import { StyledEngineProvider } from '@mui/material/styles';
+import Sidebar from './components/Sidebar/Sidebar';
 import reportWebVitals from './reportWebVitals';
-
+import GroupExample from './components/LatestNews/LatestNews';
+import LoanAdvice from './components/LoanAdvice/LoanAdvice';
+import GroupExampleF from './components/FinancialContent/FinancialContent';
+import BankingSuggestions from './components/BankingSuggestions/BankingSuggestions';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  
   <React.StrictMode>
-    <App />
+    {/* <GroupExample />
+    <GroupExampleF />
+    <BankingSuggestions />
+    <LoanAdvice /> */}
+<Sidebar />
+
+
   </React.StrictMode>
 );
+
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
