43f53768dda4c2383c4a93ff6ab8e53f711afa8d
briyeo007 <84357592+briyeo007@users.noreply.github.com>
2023-10-28 16:55:50 -0700

    Update App.js

--- frontend/client/src/App.js
+++ frontend/client/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Statement from './pages/Statement/Statement';
+import Overview from './pages/Overview/Overview';
 
-function App() {
+
+ function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Statement />} />
+        <Route path="/Overview" element={<Overview />} />
+      </Routes>
+    </Router>
   );
+
 }
 
 export default App;
