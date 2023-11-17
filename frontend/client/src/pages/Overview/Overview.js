1a0914f770ec81c561097679cacb493daa28185b
briyeo007 <84357592+briyeo007@users.noreply.github.com>
2023-11-17 21:11:11 +0800

    Overview styling and lines fixed

--- frontend/client/src/pages/Overview/Overview.js
+++ frontend/client/src/pages/Overview/Overview.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import Sidebar from '../../components/Sidebar/Sidebar';
 
-
 function Overview() {
   const currentBillAmount = 120.0;
 
@@ -51,10 +50,7 @@ function Overview() {
     '@media (max-width: 500px)': {
       flexDirection: 'column',
       alignItems: 'center',
-    
     },
-
-        
   };
 
   useEffect(() => {
@@ -87,23 +83,17 @@ function Overview() {
     { label: 'Logout', to: '/' },
   ];
 
-  
-
   return (
-    <div 
-      style={{ 
+    <div
+      style={{
         display: 'flex',
-         ...responsiveStyles, 
-         backgroundColor: 'white', 
-         justifyContent: 'center',
-         alignItems: 'center', 
-         minHeight: isMobileView ? 'auto' : '125vh',
-        
-         
-         
-          }}>
-            
-
+        ...responsiveStyles,
+        backgroundColor: 'white',
+        justifyContent: 'center',
+        alignItems: 'center',
+        minHeight: isMobileView ? 'auto' : '125vh',
+      }}
+    >
       {/* Header for Mobile View */}
       {isMobileView && (
         <div
@@ -117,12 +107,9 @@ function Overview() {
             textAlign: 'center',
             padding: '10px 0',
             zIndex: '1000',
-            height: "60px"
+            height: '60px',
           }}
         >
-          
-          
-          
           <button
             style={{
               position: 'fixed',
@@ -141,14 +128,11 @@ function Overview() {
             ☰
           </button>
         </div>
-  
       )}
-
 
       {/* Hamburger menu button (visible in responsive view) */}
       {isMobileView && (
         <>
-          
           {/* Conditional rendering for the dropdown menu */}
           {isDropdownOpen && (
             <div
@@ -179,108 +163,182 @@ function Overview() {
 
       <div style={{ display: 'flex', ...responsiveStyles }}>
         {/* Sidebar */}
-        <div id="sidebar" style={{ display: isMobileView ? 'none' : 'block', backgroundColor: 'white' }}>
+        <div
+          id="sidebar"
+          style={{
+            display: isMobileView ? 'none' : 'block',
+            backgroundColor: 'white',
+          }}
+        >
           <Sidebar />
         </div>
 
-        <div style={{ marginRight: '50px', marginTop: '340px', marginLeft: '50px', }}>
-        {isMobileView && <div style={{ height: '170px' }} />}
-        <h1>Overview</h1>
+        <div
+          style={{
+            marginRight: isMobileView ? '50px' : '150px',
+            marginTop: isMobileView ? '50px' : '10px',
+            marginLeft: '50px',
+            maxWidth: isMobileView ? '100%' : '1200px', // Set maximum width for content
+            width: 'auto', // Make content expand to full width
+          }}
+        >
+          {isMobileView && <div style={{ height: '300px' }} />}
+          <h1 style={{ marginTop: isMobileView ? '250px' : '0px' }}>Overview</h1> {/* Adjust the marginTop here */}
 
-          <h2 style={{ marginTop: '60px', marginBottom: '30px' }}>Current Bill Details</h2>
+          <h2 style={{ marginTop: '50px', marginBottom: '20px' }}>Current Bill Details</h2>
           <p>
             The estimated amount of your current bill is ${currentBillAmount.toFixed(2)} and it's due on 06/10/2022.
           </p>
-          <button className="submit-1X-small" type="submit" style={{ marginBottom: '30px' }}><Link to="/Pay" className='no-underline-link'>Pay</Link>
-            
+          <button className="submit-1X-small" type="submit" style={{ marginBottom: '10px' }}>
+            <Link to="/Pay" className="no-underline-link">
+              Pay
+            </Link>
           </button>
 
-          <div style={{ marginTop: '20px', marginBottom: '60px' }}>
+          
+          <div style={{  flex: '1', marginBottom: '20px', display: isMobileView ? 'block' : 'flex', alignItems: 'center'}}>
+      <div
+        style={{
+          flex: '1',
+          border: '1px solid #ccc',
+          padding: '20px',
+          borderRadius: '10px',
+          backgroundColor: '#f0f0f0',
+          boxShadow: '0px 4px 4px 2px rgba(0, 0, 0, 0.25)',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          width: isMobileView ? '400px' : '500px' ,
+        }}
+      >
+            
             <h2 style={{ marginBottom: '50px' }}>{editingIndex !== -1 ? 'Edit Payment' : 'Edit Payment'}</h2>
             <div
               style={{
                 display: 'flex',
                 flexDirection: 'column',
-                border: '1px solid #ccc',
-                borderRadius: '10px',
-                padding: '20px',
-                backgroundColor: '#f0f0f0',
-                boxShadow: '0px 4px 4px 2px rgba(0, 0, 0, 0.25)'
-                
               }}
             >
-              <div style={{ marginBottom: '20px', display: 'flex', flexDirection: 'column' }}>
-                <div style={{ display: 'flex', marginBottom: '10px' }}>
-                  <label htmlFor="cardholderName" style={{ flexBasis: '150px', marginRight: '10px' }}>
-                    Cardholder Name:
-                  </label>
-                  <input
-                    type="text"
-                    id="cardholderName"
-                    placeholder="Enter cardholder name"
-                    value={cardholderName}
-                    onChange={(e) => setCardholderName(e.target.value)}
-                    style={{ flex: '1', padding: '5px', border: '1px solid #ddd' }}
-                  />
-                </div>
-                <div style={{ display: 'flex', marginBottom: '10px' }}>
-                  <label htmlFor="accountNumber" style={{ flexBasis: '150px', marginRight: '10px' }}>
-                    Account Number:
-                  </label>
-                  <input
-                    type="text"
-                    id="accountNumber"
-                    placeholder="Enter account number"
-                    value={accountHolder}
-                    onChange={(e) => setAccountHolder(e.target.value)}
-                    style={{ flex: '1', padding: '5px', border: '1px solid #ddd' }}
-                  />
-                </div>
-                <div style={{ display: 'flex', marginBottom: '10px' }}>
-                  <label htmlFor="expiryDate" style={{ flexBasis: '150px', marginRight: '10px' }}>
-                    Expiry Date:
-                  </label>
-                  <input
-                    type="text"
-                    id="expiryDate"
-                    placeholder="Enter expiry date"
-                    value={expiryDate}
-                    onChange={(e) => setExpiryDate(e.target.value)}
-                    style={{ flex: '1', padding: '5px', border: '1px solid #ddd' }}
-                  />
-                </div>
-                <div style={{ display: 'flex', marginBottom: '10px' }}>
-                  <label htmlFor="cvv" style={{ flexBasis: '150px', marginRight: '10px' }}>
-                    CVV:
-                  </label>
-                  <input
-                    type="text"
-                    id="cvv"
-                    placeholder="Enter CVV"
-                    value={cvv}
-                    onChange={(e) => setCvv(e.target.value)}
-                    style={{ flex: '1', padding: '5px', border: '1px solid #ddd' }}
-                  />
-                </div>
+              <div style={{ display: 'flex', marginBottom: '10px' }}>
+                <label htmlFor="cardholderName" style={{ flexBasis: '150px', marginRight: '10px' }}>
+                  Cardholder Name:
+                </label>
+                <input
+                  type="text"
+                  id="cardholderName"
+                  placeholder="Enter cardholder name"
+                  value={cardholderName}
+                  onChange={(e) => setCardholderName(e.target.value)}
+                  style={{ flex: '1', padding: '5px', border: '1px solid #ddd' }}
+                />
               </div>
-              <button className="submit-1X-small" type="submit" onClick={handleDonePayment}>
-                {editingIndex !== -1 ? 'Done' : 'Done' }
-              </button>
+              <div style={{ display: 'flex', marginBottom: '10px' }}>
+                <label htmlFor="accountNumber" style={{ flexBasis: '150px', marginRight: '10px' }}>
+                  Account Number:
+                </label>
+                <input
+                  type="text"
+                  id="accountNumber"
+                  placeholder="Enter account number"
+                  value={accountHolder}
+                  onChange={(e) => setAccountHolder(e.target.value)}
+                  style={{ flex: '1', padding: '5px', border: '1px solid #ddd' }}
+                />
+              </div>
+              <div style={{ display: 'flex', marginBottom: '10px' }}>
+                <label htmlFor="expiryDate" style={{ flexBasis: '150px', marginRight: '10px' }}>
+                  Expiry Date:
+                </label>
+                <input
+                  type="text"
+                  id="expiryDate"
+                  placeholder="Enter expiry date"
+                  value={expiryDate}
+                  onChange={(e) => setExpiryDate(e.target.value)}
+                  style={{ flex: '1', padding: '5px', border: '1px solid #ddd' }}
+                />
+              </div>
+              <div style={{ display: 'flex', marginBottom: '10px' }}>
+                <label htmlFor="cvv" style={{ flexBasis: '150px', marginRight: '10px' }}>
+                  CVV:
+                </label>
+                <input
+                  type="text"
+                  id="cvv"
+                  placeholder="Enter CVV"
+                  value={cvv}
+                  onChange={(e) => setCvv(e.target.value)}
+                  style={{ flex: '1', padding: '5px', border: '1px solid #ddd' }}
+                />
+              </div>
             </div>
+            <button className="submit-1X-small" type="submit" onClick={handleDonePayment}>
+              {editingIndex !== -1 ? 'Done' : 'Done'}
+            </button>
           </div>
 
-          <h2 style={{ marginBottom: '50px' }}>Payment Methods</h2>
-          {paymentMethods.map((method, index) => (
-            <div key={index} style={{ marginBottom: '0px', border: '1px solid #ccc', padding: '10px', borderRadius: '10px', boxShadow: '0px 4px 4px 2px rgba(0, 0, 0, 0.25)' }}>
-              <p>{method.type}</p>
-              <p>{method.number}</p>
-              <button
-                className="submit-1X-small" type="submit" onClick={() => handleEditPayment(index)}
-              >
-                Edit Payment
-              </button>
-            </div>
-          ))}
+          <div style={{ flex: '1', marginBottom: '20px', display: isMobileView ? 'block' : 'flex', alignItems: 'center',  padding: isMobileView ? '50px' : '100px' ,}}>
+          <div
+        style={{
+          flex: '1',
+          border: '1px solid #ccc',
+          borderRadius: '10px',
+          backgroundColor: '#f0f0f0',
+          boxShadow: '0px 4px 4px 2px rgba(0, 0, 0, 0.25)',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          padding: '20px', // Add padding to create space
+          width: isMobileView ? '300px' : '500px' ,
+        }}>
+            <h2 style={{ marginBottom: '20px' }}>Payment Methods</h2>
+<div
+  style={{
+    display: 'flex',
+    flexDirection: 'column',
+    textAlign: 'center',
+    justifyContent:'center',
+    alignItems: isMobileView ? 'center' : 'flex-start',
+    gap: isMobileView ? '30px' : '0',
+  }}
+>
+  {paymentMethods.map((method, index) => (
+    <div
+      key={index}
+      style={{
+        
+        padding: '10px',
+        border: '1px solid #ccc',
+        borderRadius: '10px',
+        boxShadow: '0px 4px 4px 2px rgba(0, 0, 0, 0.25)',
+        textAlign: 'center',
+        marginBottom: '10px',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        width: isMobileView ? '250px' : '300px' ,
+      }}
+    >
+      <p>{method.type}</p>
+      <p>{method.number}</p>
+      <button
+        className="submit-1X-small"
+        type="submit"
+        onClick={() => handleEditPayment(index)}
+        style={{ marginTop: '10px',  }}
+      >
+        Edit Payment
+      </button>
+    </div>
+  ))}
+</div>
+</div>
+</div>
+</div>
+
+
+
+
 
           <Link to="/"></Link>
         </div>
