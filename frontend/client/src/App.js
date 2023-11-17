e14ef72c1f2918cf95c40b6f49c0864b216bed25
briyeo007 <84357592+briyeo007@users.noreply.github.com>
2023-11-17 09:41:22 +0800

    updated 404 that works with disconnected

--- frontend/client/src/App.js
+++ frontend/client/src/App.js
@@ -1,16 +1,13 @@
-
 import React, { useState } from "react";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";                      //firebase authentication
-import { app } from './firebase'                                                              //firebase
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";                    //React-Router-DOM
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { app } from './firebase';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
-//import Navbar from './components/navbar/Navbar'
 import LoanAdvice from './components/LoanAdvice/LoanAdvice';
 import LatestNews from './components/LatestNews/LatestNews';
-
-import Login from "./components/UserAuthentication/Login"
-import Register from "./components/UserAuthentication/Register"
-import MyProfile from "./components/MyProfile"
+import Login from "./components/UserAuthentication/Login";
+import Register from "./components/UserAuthentication/Register";
+import MyProfile from "./components/MyProfile";
 import LoanList from "./components/LoanTest/LoanList";
 import Sidebar from './components/Sidebar/Sidebar';
 import MyProfileSettings from "./components/MyProfileSettings";
@@ -21,11 +18,9 @@ import News from "./components/News/News";
 import LoanCalculator from "./components/LoanCal";
 import Overview from "./pages/Overview/Overview";
 import NotFound from "./pages/NotFound/NotFound";
+import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary';
 
-
-const auth = getAuth(app);                                                                    //firebase authentication
-
-
+const auth = getAuth(app);
 
 function App() {
   const [currentForm, setCurrentForm] = useState('login');
@@ -33,7 +28,8 @@ function App() {
   const toggleForm = (forName) => {
     setCurrentForm(forName);
   }
-  const signupPgae = () => {
+
+  const signupPage = () => {
     createUserWithEmailAndPassword(
       auth, 
       "Noyal Godara",
@@ -43,47 +39,32 @@ function App() {
   }
 
   return (
-    
-    
-    
-  
-       <>
-         <Router>
-          
-           <div className="App">
-           { <Routes>
-             { currentForm === "login" ? (
+    <Router>
+      <div className="App">
+        <ErrorBoundary>
+          <Routes>
+            {currentForm === "login" ? (
               <Route path="/" element={<Login onFormSwitch={toggleForm} />} />
-             ) : (
-               <Route path="/" element={<Register onFormSwitch={toggleForm} />} />
-             )}
-               <Route path="/Sidebar" element={<Sidebar/>} />
-               <Route path="/MyProfile" element={<MyProfile/>} />
-               <Route path="/MyProfileSettings" element={<MyProfileSettings/>} />
-               <Route path="/PaymentMethod" element={<PaymentMethod/>} />
-               <Route path="/Statement" element={<Statement/>} />
-               <Route path="/Pay" element={<Pay/>} />
-               <Route path="/LoanList" element={<LoanList/>} />
-               <Route path="/News" element={<News newsType="loan-news"/>} />
-               <Route path="/LoanCalculator" element={<LoanCalculator/>} />
-               <Route path="/LoanAdvice" element={<LoanCalculator/>} />
-               <Route path="/Overview" element={<Overview/>} />
-               <Route path="*" element={<NotFound />} />
-            
-          </Routes> }
-  
-          
-  
-          
-  
-  
-  
-             </div>
-         </Router>
-       </>
-  
-
-
+            ) : (
+              <Route path="/" element={<Register onFormSwitch={toggleForm} />} />
+            )}
+            {/* Define your other routes here */}
+            <Route path="/Sidebar" element={<Sidebar />} />
+            <Route path="/MyProfile" element={<MyProfile />} />
+            <Route path="/MyProfileSettings" element={<MyProfileSettings />} />
+            <Route path="/PaymentMethod" element={<PaymentMethod />} />
+            <Route path="/Statement" element={<Statement />} />
+            <Route path="/Pay" element={<Pay />} />
+            <Route path="/LoanList" element={<LoanList />} />
+            <Route path="/News" element={<News newsType="loan-news" />} />
+            <Route path="/LoanCalculator" element={<LoanCalculator />} />
+            <Route path="/LoanAdvice" element={<LoanCalculator />} />
+            <Route path="/Overview" element={<Overview />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </ErrorBoundary>
+      </div>
+    </Router>
   );
 }
 
