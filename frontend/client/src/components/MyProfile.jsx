994d5bc8cbc9bb4f0ebc760a4e3bf4e9bb569253
bishnoinoyal <ngodara01@mylangara.ca>
2023-12-08 00:55:39 -0800

    some updates in Sidebar

--- frontend/client/src/components/MyProfile.jsx
+++ frontend/client/src/components/MyProfile.jsx
@@ -3,31 +3,19 @@ import { Link } from "react-router-dom";
 import loanEaseLogo from "../LoanEaseLogo-White.png";
 import user from "../User.png";
 import MyProfileSettings from "./MyProfileSettings";
-import PaymentMethod from "./Pay/PaymentMethod";
-import Pay from "./Pay/Pay";
-import LoanList from "./LoanTest/LoanList";
-import NewsList from "./NewsList";
-import Sidebar from "./Sidebar/Sidebar";
-import '../App.css'
+
+import "./MyProfile.css";
 
 const MyProfile = (props) => {
-
   return (
-        <div className="container-fluid">
-
-      <div className="row">
-        <div className="col-lg-2 col-md-3" style={{ paddingLeft: 0 }}>
-          <Sidebar />
-        </div>
-        <div className="col-lg-10 col-md-9 p-0">
     <>
       <div className="welcomeMsg">
-        <img src={loanEaseLogo} height="100" width="200"/>
+        <img src={loanEaseLogo} height="100" width="200" />
         <div>
-            <h1>My Profile</h1>
-            <img src={user} height="150" width="150"/>
-            <h4>James Forbes</h4>
-            <p>BCIT</p>
+          <h1>My Profile</h1>
+          <img src={user} height="150" width="150" />
+          <h4>James Forbes</h4>
+          <p>BCIT</p>
         </div>
         <div className="welcomeMsg">
           <h4>Contact</h4>
@@ -39,11 +27,7 @@ const MyProfile = (props) => {
       <div className="auth-form-container">
         <hr />
         <h3>About Me:-</h3>
-        <p>
-          A highly motivated person, ambitious to develop experiences on
-          discovering skills and pursuing to get scholarship for achieving
-          career goals. Good at academic skills and athlete.
-        </p>
+        <p>A highly motivated person, ambitious to develop experiences on discovering skills and pursuing to get scholarship for achieving career goals. Good at academic skills and athlete.</p>
         {/* <h3>Skills:-</h3>
         <ul>
           <li>UI/UX Design</li>
@@ -52,21 +36,52 @@ const MyProfile = (props) => {
           <li>Multitasker</li>
           <li>Public Speaking</li>
         </ul> */}
-    
-        <button className="submit-3X" type="submit-3X"> <Link to="/MyProfileSettings" className="no-underline-link">Profile</Link></button>
-        <button className="submit-3X" type="submit-3X"> <Link to="/PaymentMethod" className="no-underline-link">Payment Method</Link></button>
-        <button className="submit-3X" type="submit-3X"> <Link to="/LoanList" className="no-underline-link">List of Loans</Link></button>
-        <button className="submit-3X" type="submit-3X"> <Link to="/NewsList" className="no-underline-link">NewsList</Link></button>
-        <button className="submit-3X" type="submit-3X"> <Link to="/LoanCalculator" className="no-underline-link">LoanCalculator</Link></button>
-        <button className="submit-3X" type="submit-3X"> <Link to="/Statement" className="no-underline-link">Statements</Link></button>
-        <button className="submit-3X" type="submit-3X"> <Link to="/Pay" className="no-underline-link">My Pay</Link></button>
 
+        <button className="submit-3X" type="submit-3X">
+          {" "}
+          <Link to="/MyProfileSettings" className="no-underline-link">
+            Profile
+          </Link>
+        </button>
+        <button className="submit-3X" type="submit-3X">
+          {" "}
+          <Link to="/PaymentMethod" className="no-underline-link">
+            Payment Method
+          </Link>
+        </button>
+        <button className="submit-3X" type="submit-3X">
+          {" "}
+          <Link to="/LoanList" className="no-underline-link">
+            List of Loans
+          </Link>
+        </button>
+        <button className="submit-3X" type="submit-3X">
+          {" "}
+          <Link to="/NewsList" className="no-underline-link">
+            NewsList
+          </Link>
+        </button>
+        <button className="submit-3X" type="submit-3X">
+          {" "}
+          <Link to="/LoanCalculator" className="no-underline-link">
+            LoanCalculator
+          </Link>
+        </button>
+        <button className="submit-3X" type="submit-3X">
+          {" "}
+          <Link to="/Statement" className="no-underline-link">
+            Statements
+          </Link>
+        </button>
+        <button className="submit-3X" type="submit-3X">
+          {" "}
+          <Link to="/Pay" className="no-underline-link">
+            My Pay
+          </Link>
+        </button>
       </div>
     </>
-    </div>
-    </div>
-    </div>
   );
 };
 
-export default MyProfile
+export default MyProfile;
--- frontend/client/src/components/Sidebar/Sidebar.jsx
+++ frontend/client/src/components/Sidebar/Sidebar.jsx
@@ -1,81 +1,76 @@
-import React, { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
-import { FaBars } from 'react-icons/fa';
-import { AiOutlineClose } from 'react-icons/ai';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { FaBars } from "react-icons/fa";
+import { AiOutlineClose } from "react-icons/ai";
 
-import { SidebarData  } from './SidebarData'
+import { SidebarData } from "./SidebarData";
 import "./Sidebar.css";
-import { IconContext } from 'react-icons/lib'
-import LoanEaseLogoSmall from '../../LoanEaseLogo-sm.png'
-import user from './Ellipse.png'
-import LoanEaseLogoWhite from '../../LoanEaseLogo-White.png'
-
+import { IconContext } from "react-icons/lib";
+import LoanEaseLogoSmall from "../../LoanEaseLogo-sm.png";
+import user from "./Ellipse.png";
+import LoanEaseLogoWhite from "../../LoanEaseLogo-White.png";
 
 const Sidebar = () => {
-  const [sidebar, setSidebar] = useState(false)
+  const [sidebar, setSidebar] = useState(false);
 
-  const showSidebar = () => setSidebar(!sidebar)
+  const showSidebar = () => setSidebar(!sidebar);
 
   useEffect(() => {
     const handleResize = () => {
       setSidebar(window.innerWidth >= 768);
     };
 
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
 
     return () => {
-      window.removeEventListener('resize', handleResize);
+      window.removeEventListener("resize", handleResize);
     };
   }, []);
 
   return (
     <>
-      <IconContext.Provider value={{color: 'white'}} style={{ zIndex: 1000 }}>
+      <IconContext.Provider value={{ color: "white" }} style={{ zIndex: 1000 }}>
         <div className="navbar d-flex justify-content-between px-4 d-non d-md-none">
-          <Link to="#" className='menu-bars p-2'>
-          <img           
-            src={LoanEaseLogoSmall}
-            className="d-inline-block align-top logo_LoanEase"
-            alt="LoanEase"
-          />
+          <Link to="#" className="menu-bars p-2">
+            <img src={LoanEaseLogoSmall} className="d-inline-block align-top logo_LoanEase" alt="LoanEase" />
           </Link>
-          <Link to="#" className='menu-bars p-2 '>
-            <FaBars onClick={showSidebar}/>
+          <Link to="#" className="menu-bars p-2 ">
+            <FaBars onClick={showSidebar} />
           </Link>
         </div>
-        <nav className={`${sidebar ? 'nav-menu active' : 'nav-menu'} lg-screen`} style={{ zIndex: 1000 }}>
-            <ul className="nav-menu-items p-0 h-100 nav-menu-ul" onClick={showSidebar}>
-              <ul className='m-0 p-0 d-flex flex-column justify-content-around'>
-                <li className='sm-screen'></li>
-                <li className="navbar-toggle px-4 d-flex justify-content-end d-non d-md-none">
-                  <Link to="#" className='menu-bars'>
-                    <AiOutlineClose />
-                  </Link>
-                </li>
-                <li class="text-center p-3 rounded sm-screen">
-                    <img src={user} alt="LoanEaseLogo" class="img-fluid rounded-circle"/>
-                </li>
-                <ul className='m-0 p-0'>
-                  {SidebarData.map((item, index) => {
-                    return (
-                      <li key={index} className={item.className}>
-                        <Link to={item.path}>
-                          {item.icon}
-                          <span>{item.title}</span>
-                        </Link>
-                      </li>
-                    )
-                  })}
-                </ul>
-              </ul>
-              <li class="text-center p-4 pb-2 sm-screen">
-                  <img src={LoanEaseLogoWhite} alt="LoanEaseLogo" class="img-fluid" style={{maxWidth: "100px", marginBottom: '10px'}}/>
+        <nav className={`${sidebar ? "nav-menu active" : "nav-menu"} lg-screen`} style={{ zIndex: 1000 }}>
+          <ul className="nav-menu-items p-0 h-100 nav-menu-ul" onClick={showSidebar}>
+            <ul className="m-0 p-0 d-flex flex-column justify-content-around">
+              <li className="sm-screen"></li>
+              <li className="navbar-toggle px-4 d-flex justify-content-end d-non d-md-none">
+                <Link to="#" className="menu-bars">
+                  <AiOutlineClose />
+                </Link>
               </li>
+              <li class="text-center p-3 rounded sm-screen">
+                <img src={user} alt="LoanEaseLogo" class="img-fluid rounded-circle" />
+              </li>
+              <ul className="m-0 p-0">
+                {SidebarData.map((item, index) => {
+                  return (
+                    <li key={index} className={item.className}>
+                      <Link to={item.path}>
+                        {item.icon}
+                        <span>{item.title}</span>
+                      </Link>
+                    </li>
+                  );
+                })}
+              </ul>
             </ul>
+            <li class="text-center p-4 pb-2 sm-screen">
+              <img src={LoanEaseLogoWhite} alt="LoanEaseLogo" class="img-fluid" style={{ maxWidth: "100px", marginBottom: "10px" }} />
+            </li>
+          </ul>
         </nav>
       </IconContext.Provider>
     </>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
